d0d4f3e84b05b7d2afdb00cede83f65096c9963c
rohitpawar770 <rohit.p770@gmail.com>
2021-05-03 18:27:02 +0530

    my first commit

---
+++ automation.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://www.youtube.com')
+
+searchBox = driver.find_element_by_xpath('//*[@id="search"]')
+searchBox.send_keys("BTS on song")
+
+searchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+
+searchButton.click()
+
+firstVideo = driver.find_element_by_xpath('//*[@id="img"]').click()
+
